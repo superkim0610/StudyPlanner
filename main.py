34f6903aaab7af25c3e03bcb1690418600762b9e
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2023-02-28 04:21:18 +0900

    sTime operating added, studyingTime added

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
 import time
 
-class studyTime:
+class sTime:
     """
     Data type class used to define time.\n
 
     <Set time to given time>\n
-    * studyTime(yyyy, mm, dd, hour, min, sec)\n
-    * studyTime().setTime(yyyy, mm, dd, hour, min, sec)\n
+    * sTime(yyyy, mm, dd, hour, min, sec)\n
+    * sTime().setTime(yyyy, mm, dd, hour, min, sec)\n
 
     <Set time to present time>\n
-    * studyTime(now=True)\n
-    * studyTime().setTime(now=True)\n
+    * sTime(now=True)\n
+    * sTime().setTime(now=True)\n
+
+    <Set time by tuple data>\n
+    * sTime(tuple_data)\n
+    * sTime().setTime(tuple_data)\n
 
     <Initialize time>\n
-    * studyTime()\n
-    * studyTime().setTime()\n
+    * sTime()\n
+    * sTime().setTime()\n
 
-    <Get time data>\n
-    * studyTime.time() -> (yyyy, mm, dd, hour, min, sec)
+    <Return time data>\n
+    * sTime.time() -> (yyyy, mm, dd, hour, min, sec)
     """
 
-    def __init__(self, year:int=0, mon:int=0, mday:int=0, hour:int=0, minute:int=0, sec:int=0, now:bool=False):
-        self.setTime(year, mon, mday, hour, minute ,sec, now=now)
+    def __init__(self, year:int=0, mon:int=0, mday:int=0, hour:int=0, minute:int=0, sec:int=0, data:tuple=None, now:bool=False):
+            self.setTime(year, mon, mday, hour, minute ,sec, data=data, now=now)
 
     def __repr__(self) -> str:
         return f"<{self.year}-{self.mon:02d}-{self.mday:02d} {self.hour:02d}:{self.minute:02d}:{self.sec:02d}>"
     
     def __eq__(self, other:object):
         return self.time() == other.time()
+    
+    def __add__(self, other:object):
+        s = self.time()
+        o = other.time()
+        r = (s[i] + o[i] for i in range(6))
+        return sTime(data=r)
 
-    def setTime(self, year:int=0, mon:int=0, mday:int=0, hour:int=0, minute:int=0, sec:int=0, now:bool=False):
+    def __sub__(self, other:object):
+        s = self.time()
+        o = other.time()
+        r = (s[i] - o[i] for i in range(6))
+        return sTime(data=r)
+
+    def setTime(self, year:int=0, mon:int=0, mday:int=0, hour:int=0, minute:int=0, sec:int=0, data:tuple=None, now:bool=False):
         if now == True:
             t = time.localtime()
             self.setTime(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min ,t.tm_sec)
+        elif data is not None:
+            self.setTime(*data)
         else:
             self.year  = year
             self.mon = mon
@@ -45,22 +63,47 @@ class studyTime:
         return (self.year, self.mon, self.mday, self.hour, self.minute, self.sec)
 
 class studyEvent:
-    def __init__(self, subjectName:str, startTime:studyTime=studyTime(), finishTime:studyTime=studyTime()):
+    """
+    class studyEvent is the object can save the data about study event.
+    for example, subject name, start time, finish time, comment
+    """
+    def __init__(self, subjectName:str, startTime:sTime=sTime(), finishTime:sTime=sTime(), comment:list=[]):
         self.subjectName = subjectName
         self.startTime = startTime
         self.finishTime = finishTime
+        self.studyingTime = self.finishTime - self.startTime
+        self.comment = comment
 
-    def setSubjectName(self, subjectName):
+    def setSubjectName(self, subjectName:str):
         self.setSubjectName = subjectName
 
-    def setStartTime(self):
-        pass
+    def setStartTime(self, startTime:sTime=sTime(), now:bool=False):
+        if now == True:
+            self.startTime = sTime(now=True)
+        else:
+            self.startTime = startTime
+        self.studyingTime = self.finishTime - self.startTime
 
-    def setFinishTime(self):
-        pass
+    def setFinishTime(self, finishTime:sTime=sTime(), now:bool=False):
+        if now == True:
+            self.finishTime = sTime(now=True)
+        else:
+            self.finishTime = finishTime
+        self.studyingTime = self.finishTime - self.startTime
+
+    def addComment(self, commentName:str, commentContent:str=""):
+        """
+        for example, comment = [['name1', 'content1'], ['name2', 'content2'], ...]
+        """
+        self.comment.append([commentName, commentContent])
 
 def main():
-    pass
+    # test (sTime operating)
+    a = sTime(data=(0,1,2,3,4,5))
+    b = sTime()
+    b.setTime(data=(3,5,6,7,8,9))
+    print(b-a)
+    print((b-a).time())
 
 if __name__ == "__main__":
     main()
